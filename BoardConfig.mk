a4c28d5c10c4804ebe069c1740378faab68ad256
Markus Heuberger <markus.heuberger@gmx.net>
2025-09-11 12:32:57 +0000

    Revert "cmi: Set vendor init lib via soong config"
    This reverts commit 820e726b5d3b66fd08ebfc104bb8ada023fccdb1.
    
    Reason for revert: This should not be migrated to soong config as long as https://github.com/LineageOS/android_device_xiaomi_sm8250-common/blob/lineage-22.2/BoardConfigCommon.mk still uses the deprecated way to set it to //$(COMMON_PATH):init_xiaomi_kona
    
    Change-Id: I8b18f229c756589996f1d648a47b7721a7012f3d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,9 @@ DEVICE_PATH := device/xiaomi/cmi
 # Display
 TARGET_SCREEN_DENSITY := 440
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_cmi
+
 # Kernel
 TARGET_KERNEL_CONFIG += vendor/xiaomi/cmi.config
 
